ca3141aca26ef491c12b308bff24541c85c89acb
kavi-1501 <kavitha1501@yahoo.com>
2025-08-17 11:11:45 +0530

    Create rg.tf

---
+++ rg.tf
@@ -0,0 +1,36 @@
+terraform { 
+  cloud { 
+    
+    organization = "kavitha1501" 
+
+    workspaces { 
+      name = "localfile" 
+    } 
+  } 
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+features {}
+client_id="c391aa41-99a3-4c14-acac-31ff4e52a905"
+client_secret="VuA8Q~R1vxHLr7RclqD3bscEqlpWi~GpFTkK-a9y"
+tenant_id="7597b8dc-1559-4285-901d-856b7ac20009"
+subscription_id="6babc3f8-793c-4200-9aa3-51e8d33ff572"
+}
+variable "rgname" {
+  default = "terraformrgkavi"
+}
+variable "location" {
+  default = "West US 3"
+}
+resource "azurerm_resource_group" "example" {
+  name     = var.rgname
+  location = var.location
+}
